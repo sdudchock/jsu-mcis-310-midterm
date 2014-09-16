f2b3f073e42bea68ec05da3f2ef2fc14b4e8082f
sdudchock <sdudchock@jsu.edu>
2014-09-16 14:00:49 -0500

    Saving with 25 minutes left.
    Tried to implement some of the tests.

--- Hangman/src/main/java/edu/jsu/mcis/Hangman.java
+++ Hangman/src/main/java/edu/jsu/mcis/Hangman.java
@@ -6,6 +6,8 @@ public class Hangman {
     public enum Result { WIN, LOSE, NONE };
     private String word;
     private List<Character> usedLetters;
+	private int i = 0;
+	private int guessNumber = 0;
     
     public Hangman() {
         word = "";
@@ -17,11 +19,36 @@ public class Hangman {
     }
     
     public boolean available(char c) {
-        return true;
+		if (i < word.length()){
+			if (c == word.charAt(i)){
+				i++;
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			i = 0;
+			return false;
+		}
     }
     
     public int guess(char c) {
-        return 2;
+		int hits = 0;
+		usedLetters.add(c);
+		for (i = 0;i < word.length(); i++){
+			if(c == word.charAt(i)){
+				hits++;
+			}
+		}
+		guessNumber++;
+		if (hits > 0){
+			return hits;
+		}
+        else{
+			return 0;
+		}
     }
     
     public Result getResult() {
